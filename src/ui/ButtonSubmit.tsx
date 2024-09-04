d22c8b984ed56f24da18df89b287e0db27f6342a
usercoderr <aidarali.prod@gmail.com>
2024-09-04 17:45:11 +0600

    push

--- src/ui/ButtonSubmit.tsx
+++ src/ui/ButtonSubmit.tsx
@@ -1,13 +1,14 @@
-import  {FC, ReactNode} from "react";
+import {ButtonHTMLAttributes, FC, ReactNode} from "react";
 
-type Props ={
-    children: ReactNode
-}
-const ButtonSubmit:FC<Props> = ({children}) =>{
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: ReactNode;
+};
+const ButtonSubmit:FC<Props> = ({children, ...props}) =>{
     return(
         <button
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
+            {...props}
         >
             {children}
         </button>
